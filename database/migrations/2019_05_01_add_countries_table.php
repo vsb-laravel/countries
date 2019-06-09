e76ce24e78f0c64e401e70fb6ea691aee9c252ac
Vladimir Bushuev <v.bushuev@gmail.com>
2019-06-09 20:04:14 +0300

    Update 2019_05_01_add_countries_table.php

--- database/migrations/2019_05_01_add_countries_table.php
+++ database/migrations/2019_05_01_add_countries_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCountriesTable extends Migration {
+class CountriesTable extends Migration {
 
 	/**
 	 * Run the migrations.
